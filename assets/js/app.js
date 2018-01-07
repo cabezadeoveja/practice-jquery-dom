6cc9e57ed27dc2417508260a212a36f636ed493f
nerdale <alexandra.neira@gmail.com>
2018-01-07 01:56:25 -0300

    agregando las funcionalidades faltantes

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,6 +21,7 @@ $(document).ready(() => {
 var gossip = data.gossip.characters;
 var breaking = data.breakingbad.characters;
 var howtoget = data.howtoget.characters;
+var stranger = data.stranger.characters;
 
 /*
 * Ahora comenzamos con el contenido. Cuando se le haga click al botón de Gossip girl, mostraremos
@@ -53,8 +54,8 @@ $('.btn-gossip').click(function(){
                             '<h3>' + gossip[i].name + '</h3>' +
                             '<p>School: ' + gossip[i].school + '</p>' +
                             '<p>City: ' + gossip[i].city + '</p>' +
-                            '</div>' +
-                          '</div>')
+                          '</div>' +
+                        '</div>')
   }
 })
 
@@ -69,12 +70,12 @@ $('.btn-breaking').click(function(){
   for (var i = 0; i < breaking.length; i++) {
     $('.gossip, .how').children().remove();
     $('.breaking').append('<div class="row character">' +
-                          '<div class= "col-md-6 text-center">'+
-                            '<img src="assets/' + breaking[i].picture + '">' +
-                          '</div>' +
-                          '<div class= "col-md-6">'+
-                            '<h3>' + breaking[i].name + '</h3>' +
-                            '<p>City: ' + breaking[i].city + '</p>' +
+                            '<div class= "col-md-6 text-center">'+
+                              '<img src="assets/' + breaking[i].picture + '">' +
+                            '</div>' +
+                            '<div class= "col-md-6">'+
+                              '<h3>' + breaking[i].name + '</h3>' +
+                              '<p>City: ' + breaking[i].city + '</p>' +
                             '</div>' +
                           '</div>')
   }
@@ -84,13 +85,77 @@ $('.btn-how').click(function(){
   for (var i = 0; i < howtoget.length; i++) {
     $('.gossip, .breaking').children().remove();
     $('.how').append('<div class="row character">' +
-                          '<div class= "col-md-6 text-center">'+
-                            '<img src="assets/' + howtoget[i].picture + '">' +
-                          '</div>' +
-                          '<div class= "col-md-6">'+
-                            '<h3>' + howtoget[i].name + '</h3>' +
-                            '<p>City: ' + howtoget[i].city + '</p>' +
-                            '</div>' +
-                          '</div>')
+                        '<div class= "col-md-6 text-center">'+
+                          '<img src="assets/' + howtoget[i].picture + '">' +
+                        '</div>' +
+                        '<div class= "col-md-6">'+
+                          '<h3>' + howtoget[i].name + '</h3>' +
+                          '<p>City: ' + howtoget[i].city + '</p>' +
+                        '</div>' +
+                      '</div>')
+  }
+})
+
+$('.btn-stranger').click(function(){
+  for (var i = 0; i < stranger.length; i++) {
+    $('.gossip, .breaking, .how').children().remove();
+    $('.stranger').append('<div class="row character">' +
+                        '<div class= "col-md-6 text-center">'+
+                          '<img src="assets/' + stranger[i].picture + '">' +
+                        '</div>' +
+                        '<div class= "col-md-6">'+
+                          '<h3>' + stranger[i].name + '</h3>' +
+                          '<p>City: ' + stranger[i].city + '</p>' +
+                        '</div>' +
+                      '</div>')
+  }
+})
+
+/*
+* Ahora haremos el evento que al presionar el botón All nos muestre todos los personajes
+* de todas las series.
+* Crearemos un arreglo vacío donde pushearemos todos los objetos de characters
+*/
+
+var allCharacters = [];
+
+$('.btn-all').click(function() {
+  $('.gossip, .breaking, .how, .stranger').children().remove();
+/*
+* Debemos recorrer cada de uno de los objetos presente en la data (gossip, breakingbad,
+* howtoget y stranger) y pushear al array allCharacters todos los objetos pertenecientes a
+* la propiedad characters
+*/
+  for (var i in data) {
+    allCharacters.push(data[i].characters);
+  }
+  /*
+  * Si hacemos console.log(allCharacters) podremos ver que ahora tenemos un arreglo con arreglos
+  * en su interior, cada arreglo interno a su vez tiene en su interior los personajes (objetos)
+  * Puedes comprobarlo haciendo console.log(allCharacters[0], allCharacters[1],
+  * allCharacters[2], allCharacters[3]), es decir, tenemos un arreglo compuesto de
+  * arreglos que en su interior se compone de objetos.
+  * Entonces para poder llegar a las propiedades de cada personaje debemos iterar paralelamente
+  * con dos for, en español esto quiere decir que estaríamos entrando por ejemplo así:
+  * allCharacters[0][0] <-- el primer par de corchetes corresponde los personajes de gossip girl,
+  * el segundo par corresponde al primer personaje de gossip girl, en este caso Blair Waldorf
+  * allCharacters[1][0] <-- el primer par de corchetes corresponde los personajes de breaking bad,
+  * el segundo par corresponde al primer personaje de breaking bad, en este caso Walter White
+  * Mientras iteramos en cada personaje, es decir dentro del segundo for, hacemos el append
+  * correspondiente
+  */
+  for (var i = 0; i < allCharacters.length; i++) {
+    for (var j = 0; j < allCharacters.length; j++) {
+    $('.all').append('<div class="row character">' +
+                        '<div class= "col-md-6 text-center">'+
+                          '<img src="assets/' + allCharacters[i][j].picture + '">' +
+                        '</div>' +
+                        '<div class= "col-md-6">'+
+                          '<h3>' + allCharacters[i][j].name + '</h3>' +
+                          '<p>School: ' + allCharacters[i][j].school + '</p>' +
+                          '<p>City: ' + allCharacters[i][j].city + '</p>' +
+                        '</div>' +
+                      '</div>')
+    }
   }
 })
